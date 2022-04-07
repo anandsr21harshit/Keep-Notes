e68e5391875df69c4fca2577c2f500b8a86fe7a3
Harshit Anand <harshitanand97@gmail.com>
2022-04-08 00:43:05 +0530

    Feat: logout feature

--- keep-notes/src/components/SideBar.jsx
+++ keep-notes/src/components/SideBar.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/side-bar.css";
 import {Link} from "react-router-dom"
+import { LogoutModal } from "./Logout-Modal";
 
 function SideBar() {
+
+  const localStorageItem = JSON.parse(localStorage.getItem("loginCred"))
+  const user = localStorageItem.user.firstName;
+
+  const [logoutModal, setLogoutModal] = useState(false);
+
   return (
+    <>
     <div className="side-bar">
       <Link to={"/home"} className="side-bar-links">
       <div className="options">
@@ -29,13 +37,15 @@ function SideBar() {
         <h3>Trash</h3>
       </div>
       </Link>
-      <Link to={"/profile"} className="side-bar-links">
-      <div className="options">
+      <Link to={"#"} className="side-bar-links">
+      <div className="options" onClick={()=>setLogoutModal(true)}>
         <i className="bi bi-person-fill"></i>
-        <h3>Profile</h3>
+        <h3>{user}</h3>
       </div>
       </Link>
     </div>
+    {logoutModal && <LogoutModal setLogoutModal={setLogoutModal}/>}
+    </>
   );
 }
 
