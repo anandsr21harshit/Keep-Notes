ca079704e5eac75458ee5f7036ec8f8ba01f7087
Harshit Anand <harshitanand97@gmail.com>
2022-04-08 00:44:33 +0530

    Edit feature added

--- keep-notes/src/components/NoteCard.jsx
+++ keep-notes/src/components/NoteCard.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useAuth, useData } from "../context";
 import "../css/note-card.css";
+import { NotesInput } from "./NotesInput";
 
 function NoteCard({ notesData }) {
-  const{token} = useAuth()
-  const{state,dispatch} = useData()
+  const { token } = useAuth();
+  const { state, dispatch } = useData();
+  const [edit, setEdit] = useState();
 
   async function deleteHandler() {
     try {
@@ -15,57 +17,86 @@ function NoteCard({ notesData }) {
         },
       });
 
-      if(response.status === 200 || response.status === 201){
-        const deletedData = state.notes.filter(item => item._id === notesData._id)[0];
-        console.log({deletedData});
-        dispatch({type:"TRASH", payload:deletedData})
-        dispatch({type:"ADD_NOTES",payload:{note: response.data.notes}})
+      if (response.status === 200 || response.status === 201) {
+        const deletedData = state.notes.filter(
+          (item) => item._id === notesData._id
+        )[0];
+        console.log({ deletedData });
+        dispatch({ type: "TRASH", payload: deletedData });
+        dispatch({ type: "ADD_NOTES", payload: { note: response.data.notes } });
       }
     } catch (err) {
       console.error(err.message);
     }
   }
 
-  async function archiveHandler(){
-    try{
-      const response = await axios.post(`/api/notes/archives/${notesData._id}`,{notesData},{
-        headers:{
-          authorization:token
+  async function archiveHandler() {
+    try {
+      const response = await axios.post(
+        `/api/notes/archives/${notesData._id}`,
+        { notesData },
+        {
+          headers: {
+            authorization: token,
+          },
         }
-      });
+      );
 
-      if(response.status === 200 || response.status === 201){
-        dispatch({type:"ARCHIVE",payload:{archive: response.data.archives}});
-        dispatch({type:"ADD_NOTES", payload:{note: response.data.notes}});
+      if (response.status === 200 || response.status === 201) {
+        dispatch({
+          type: "ARCHIVE",
+          payload: { archive: response.data.archives },
+        });
+        dispatch({ type: "ADD_NOTES", payload: { note: response.data.notes } });
       }
-    }
-    catch(err){
+    } catch (err) {
       console.error(err);
     }
   }
 
+  function editHandler() {
+    setEdit(true);
+  }
+
   return (
-    <div className="card-container">
-      <div
-        className="card card-without-image"
-        style={{ backgroundColor: notesData.backgroundColor}}
-      >
-        <div className="heading">
-          <div className="card-title">{notesData.title}</div>
-          <div className="card-sub-title label">{notesData.label}</div>
+    <>
+      {edit ? (
+        <NotesInput data={notesData} setEdit={setEdit} />
+      ) : (
+        <div className="card-container">
+          <div
+            className="card card-without-image"
+            style={{ backgroundColor: notesData.backgroundColor }}
+          >
+            <div className="heading">
+              <div className="card-title">{notesData.title}</div>
+              <div className="card-sub-title label">{notesData.label}</div>
+            </div>
+            <div className="card-content">{notesData.content}</div>
+            <footer className="card-footer">
+              <div className="d-flex">{notesData.dateCreated}</div>
+              <div className="icon-wrapper d-flex">
+                <i
+                  title="edit"
+                  className="bi bi-pencil-square m-32"
+                  onClick={editHandler}
+                ></i>
+                <i
+                  title="archive"
+                  className="bi bi-archive-fill m-32"
+                  onClick={archiveHandler}
+                ></i>
+                <i
+                  title="delete"
+                  className="bi bi-trash-fill m-32"
+                  onClick={deleteHandler}
+                ></i>
+              </div>
+            </footer>
+          </div>
         </div>
-        <div className="card-content">{notesData.content}</div>
-        <footer className="card-footer">
-          <i title="edit" className="bi bi-pencil-square m-32"></i>
-          <i title="archive" className="bi bi-archive-fill m-32" onClick={archiveHandler}></i>
-          <i
-            title="delete"
-            className="bi bi-trash-fill m-32"
-            onClick={deleteHandler}
-          ></i>
-        </footer>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
--- keep-notes/src/components/NotesInput.jsx
+++ keep-notes/src/components/NotesInput.jsx
@@ -5,13 +5,12 @@ import { useAuth, useData } from "../context/index";
 import { NoteCard } from "./NoteCard";
 import "../css/notes-input.css";
 
-function NotesInput() {
+function NotesInput({ data, setEdit }) {
   const { token } = useAuth();
   const { state, dispatch } = useData();
 
   const [colorPalette, setColorPalette] = useState(false);
   const [color, setColor] = useState();
-
 
   const date = new Date();
 
@@ -22,19 +21,32 @@ function NotesInput() {
       date.getMonth() + 1
     }/${date.getFullYear()}`,
     label: "",
-    backgroundColor: "#FFFFFF"
+    backgroundColor: "#FFFFFF",
   };
 
-  const [note, setNote] = useState(initialNote);
+  const [note, setNote] = useState(data ? data : initialNote);
 
   const addNotesHandler = async () => {
     try {
-      const response = await axios.post(
-        "/api/notes",
-        { note },
-        { headers: { authorization: token } }
-      );
+      let response = null;
 
+      if (data) {
+        response = await axios.post(
+          `/api/notes/${data._id}`,
+          { note },
+          {
+            headers: {
+              authorization: token,
+            },
+          }
+        );
+      } else {
+        response = await axios.post(
+          "/api/notes",
+          { note },
+          { headers: { authorization: token } }
+        );
+      }
       console.log(response);
 
       if (response.status === 200 || response.status === 201) {
@@ -47,73 +59,76 @@ function NotesInput() {
 
   return (
     <>
-      <div className="notes-input-container">
-        <header className="notes-header">
-          <input
-            className="notes-title"
-            type="text"
-            placeholder="Title"
-            value={note.title}
-            onChange={(e) =>
-              setNote(() => ({ ...note, title: e.target.value }))
-            }
-          />
-          <p>
-            <i className="bi bi-pin-fill"></i>
-          </p>
-        </header>
-        <section>
-          <textarea
-            placeholder="Type Notes"
-            className="notes-area"
-            value={note.content}
-            onChange={(e) =>
-              setNote(() => ({ ...note, content: e.target.value }))
-            }
-          ></textarea>
-        </section>
-        <footer className="notes-footer">
-          <div className="footer-left">
+      <div className={`${data && "modal"}`}>
+        <div className="notes-input-container">
+          <header className="notes-header">
             <input
+              className="notes-title"
               type="text"
-              placeholder="Label"
-              className="notes-label"
-              value={note.label}
+              placeholder="Title"
+              value={note.title}
               onChange={(e) =>
-                setNote(() => ({ ...note, label: e.target.value }))
+                setNote(() => ({ ...note, title: e.target.value }))
               }
             />
-            <i
-              className="bi bi-palette"
-              onClick={() => setColorPalette(!colorPalette)}
-            ></i>
-            {colorPalette && (
-              <ChromePicker
-                className="color-palette"
-                color={color}
-                onChange={(newColor) => {
-                  setColor(newColor.hex)
-                  setNote(() => ({ ...note, backgroundColor: newColor.hex }));
-                }}
+            <p>
+              <i className="bi bi-pin-fill"></i>
+            </p>
+          </header>
+          <section>
+            <textarea
+              placeholder="Type Notes"
+              className="notes-area"
+              value={note.content}
+              onChange={(e) =>
+                setNote(() => ({ ...note, content: e.target.value }))
+              }
+            ></textarea>
+          </section>
+          <footer className="notes-footer">
+            <div className="footer-left">
+              <input
+                type="text"
+                placeholder="Label"
+                className="notes-label"
+                value={note.label}
+                onChange={(e) =>
+                  setNote(() => ({ ...note, label: e.target.value }))
+                }
               />
-            )}
-          </div>
-          <button
-            className="btn btn-primary btn-add"
-            onClick={() => {
-              addNotesHandler();
-              setNote(() => ({
-                title: "",
-                content: "",
-                backgroundColor: note.backgroundColor,
-                dateCreated: note.dateCreated,
-                label: "",
-              }));
-            }}
-          >
-            Add
-          </button>
-        </footer>
+              <i
+                className="bi bi-palette"
+                onClick={() => setColorPalette(!colorPalette)}
+              ></i>
+              {colorPalette && (
+                <ChromePicker
+                  className="color-palette"
+                  color={color}
+                  onChange={(newColor) => {
+                    setColor(newColor.hex);
+                    setNote(() => ({ ...note, backgroundColor: newColor.hex }));
+                  }}
+                />
+              )}
+            </div>
+            <button
+              className="btn btn-primary btn-add"
+              onClick={() => {
+                addNotesHandler();
+                setNote(() => ({
+                  title: "",
+                  content: "",
+                  backgroundColor: note.backgroundColor,
+                  dateCreated: note.dateCreated,
+                  label: "",
+                }));
+                data && setEdit(false);
+              }}
+            >
+              Add
+            </button>
+          </footer>
+        </div>
       </div>
     </>
   );
